1f68e7a5bedd5954c1e77cbe958982dd9245577a
mkonig411blk <margot.konig@blackrock.com>
2019-08-12 18:03:18 -0500

    cleaned up routes, added nested route for teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  resources :signups
-  resources :teams
-  resources :users
+  # teams & signups
+  resources :teams, only: [:show, :index] do
+      resources :signups, only: [:new]
+  end
+  get '/myteams' => 'teams#myindex'
+  # users
+  resources :users, only: [:new, :show, :edit]
   root 'users#welcome'
-  # get '/signup' => 'users#new'
-  # post '/signup' => 'users#create'
+  # sessions
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get '/auth/facebook/callback' => 'sessions#createfb'
 
-  get '/myteams' => 'teams#myindex'
 end
